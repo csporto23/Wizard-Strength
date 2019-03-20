b9d8834922c41beb098a4476077fd891d931d48d
csporto23 <Csporto23@gmail.com>
2019-03-20 12:09:06 -0500

    modal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,16 @@
 $("#StartGame").on("click", function () {
     let random = Math.floor(Math.random() * (120 - 19) + 1) + 19;
     $("#RandomNumber").text(random);
+    
 });
+
+
+
+
+
+
+$("#items").on("click", function () {
+    let item = Math.floor(Math.random() * (12) + 1);
+    console.log(item)
+    $("#TotalScore").text(item);
+})
