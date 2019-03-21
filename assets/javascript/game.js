88db55c2b21b167f5b7dae13a2eedbf6027d4a51
csporto23 <Csporto23@gmail.com>
2019-03-20 22:17:13 -0500

    if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,25 @@
+let random = Math.floor(Math.random() * (120 - 19) + 1) + 19;
+let item = Math.floor(Math.random() * (12) + 1);
+
 $("#StartGame").on("click", function () {
-    let random = Math.floor(Math.random() * (120 - 19) + 1) + 19;
+    const totalReset = 0;
     $("#RandomNumber").text(random);
-    
+    $("#TotalScore").text(totalReset);    
 });
 
-
-
-
-
-
 $("#items").on("click", function () {
-    let item = Math.floor(Math.random() * (12) + 1);
-    console.log(item)
     $("#TotalScore").text(item);
 })
+
+if (item === random){
+    console.log("Winner!");
+} else (item > random) {
+    console.log("loser!");
+}
+
+
+if (item < random) {
+    console.log("trigger")
+} else (item === random) {
+    console.log("trigger");
+}
