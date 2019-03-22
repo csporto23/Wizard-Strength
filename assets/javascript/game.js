3602dc154ff73f6570de876db3bdab5a196fa910
csporto23 <Csporto23@gmail.com>
2019-03-22 00:49:22 -0500

    reset and if else

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,29 @@
 let random = Math.floor(Math.random() * (120 - 19) + 1) + 19;
 let userScore = 0;
+let wins = 0;
+let loses = 0;
 $("#StartGame").on("click", function () {
     
     // const totalReset = 0;
-    $("#RandomNumber").text(random);
+    $("#RandomNumber").html(random);
     $("#TotalScore").html(userScore);    
 });
 
+
+
 $("#items").bind("click", function () {
-    let item = Math.floor(Math.random() * (12) + 1);
+    var item = Math.floor(Math.random() * (12) + 1);
     userScore += item;
     $("#TotalScore").html(userScore);
 })
 
-if (item === random) {
-    console.log("winner!");
-} else {
-    console.log("loser!");
+const reset = function() {
+    userScore = 0;
+    random = 0;
+}
+
+if (userScore === random) {
+     ($("#wins").html(wins));
+} else if (userScore > random) {
+    ($("#loses").html(loses));
 }
