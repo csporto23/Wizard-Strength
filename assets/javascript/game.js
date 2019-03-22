dbff32f259e9ce50139672d9e8b583373eda7328
csporto23 <Csporto23@gmail.com>
2019-03-21 21:58:28 -0500

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
 let random = Math.floor(Math.random() * (120 - 19) + 1) + 19;
-
+let userScore = 0;
 $("#StartGame").on("click", function () {
     
-    const totalReset = 0;
+    // const totalReset = 0;
     $("#RandomNumber").text(random);
-    $("#TotalScore").html(totalReset);    
+    $("#TotalScore").html(userScore);    
 });
 
-$("#items").bind("click", function () {
+$("#banana").bind("click", function () {
     let item = Math.floor(Math.random() * (12) + 1);
-    items = item+item+item+item;
-    $("#TotalScore").html(item);
+    userScore += item;
+    $("#TotalScore").html(userScore);
 })
 
 if (item === random) {
